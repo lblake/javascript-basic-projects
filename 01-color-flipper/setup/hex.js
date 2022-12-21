3c1eaa08784207174cda21e0a3ac80e561b1ecad
lblake <treleven.lloyd@gmail.com>
2022-12-21 20:35:31 +0000

    added code to display color name,  using a call to an api endpoint with axios

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,31 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// const {default: axios} = require('axios');
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+axios
+  .get(`https://www.thecolorapi.com/id?hex=0047AB`)
+  .then((res) => {
+    console.log(res);
+    colorValue = res.data.name.value;
+    console.log('This is the value', colorValue);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+btn.addEventListener('click', function () {
+  let hexColor = '';
+
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+    console.log('This is the hex value', hexColor);
+  }
+
+  document.querySelector('.colorName').textContent = colorValue;
+  document.body.style.backgroundColor = hexColor;
+  document.querySelector('.color').textContent = '#' + hexColor;
+});
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * hex.length);
+};
