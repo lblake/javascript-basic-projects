d60682da7ce71b1c92b6a2a1dc17e20cb2dbf695
lblake <treleven.lloyd@gmail.com>
2022-12-21 23:51:23 +0000

    added code to get and set hex color name

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,29 +1,38 @@
-// const {default: axios} = require('axios');
-
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const btn = document.getElementById('btn');
 
-axios
-  .get(`https://www.thecolorapi.com/id?hex=0047AB`)
-  .then((res) => {
-    console.log(res);
-    colorValue = res.data.name.value;
-    console.log('This is the value', colorValue);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const getColorName = (colorValue) => {
+  // Remove the # from the string so it can be passed to the api endpoint
+  colorValue = colorValue.slice(1);
+
+  axios
+    .get(`https://www.thecolorapi.com/id?hex=${colorValue}`)
+    .then((res) => {
+      console.log(res);
+      colorValue = res.data.name.value;
+      console.log('The name is:', colorValue);
+      setColorName(colorValue);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const setColorName = (name) => {
+  document.querySelector('.colorName').textContent = name;
+};
 
 btn.addEventListener('click', function () {
-  let hexColor = '';
+  let hexColor = '#';
 
   for (let i = 0; i < 6; i++) {
     hexColor += hex[getRandomNumber()];
-    console.log('This is the hex value', hexColor);
   }
 
-  document.querySelector('.colorName').textContent = colorValue;
+  getColorName(hexColor);
+
+  document.querySelector('.color').textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
-  document.querySelector('.color').textContent = '#' + hexColor;
 });
 
 const getRandomNumber = () => {
